dd6e0a320b2535eceafb0be74dcf2f3a659b7f5e
sushmavp1 <68282268+sushmavp1@users.noreply.github.com>
2020-08-07 21:44:51 +0530

    Create convertion.java

---
+++ convertion.java
@@ -0,0 +1,29 @@
+package minutestoyears;
+import java.util.Scanner;
+public class convertion{
+	
+	public static void main(String[] Strings) {
+		
+		double minutesinyear = 60*24*365;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input the number of minutes:");
+		
+		
+		double min = input.nextDouble();
+		
+		long years = (long) (min / minutesinyear);
+		
+		int days = (int) (min / 60 / 24) % 365;
+		
+		System.out.println((int) min + "minutes is approximately" + years + "years and" + days +"days");
+	}
+}
+				
+				
+	
+
+
+
+
